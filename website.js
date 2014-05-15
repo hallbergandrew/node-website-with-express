a1cd11e0cbd939458db4cdb1ce289b04b3dbf575
hallbergandrew <hallbergandrew@gmail.com>
2014-05-14 18:24:34 -0700

    Added Cross-Page Testing and Logic Testing

--- website.js
+++ website.js
@@ -30,6 +30,14 @@ app.get('/about', function(req,res) {
     pageTestScript: '/qa/tests-about.js'
      } );
 });
+
+app.get('/tours/hood-river', function(req,res){
+  res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req,res){
+  res.render('tours/request-group-rate');
+});
 // catch-all handler (middleware)
 app.use(function(req,res,next){
   res.status(404);
